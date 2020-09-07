dce6ded8f38d9348a41ff726472d848efe3b45e4
Marijana Satler <marijanasatler@gmail.com>
2020-09-07 14:46:46 +0200

    Updates

---
+++ precache-manifest.f23dbb452c5c96d4192c70d40d8c918d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0432a7eef188badd2650fcbff5e67bce",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "9fe966f72e31d40241db",
+    "url": "/my-portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "0f615a0c77a9bca5758f",
+    "url": "/my-portfolio/static/css/main.a5777688.chunk.css"
+  },
+  {
+    "revision": "9fe966f72e31d40241db",
+    "url": "/my-portfolio/static/js/2.21c18d5c.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/my-portfolio/static/js/2.21c18d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f615a0c77a9bca5758f",
+    "url": "/my-portfolio/static/js/main.414496b4.chunk.js"
+  },
+  {
+    "revision": "147ade476a225cfec2f4",
+    "url": "/my-portfolio/static/js/runtime-main.7103b357.js"
+  },
+  {
+    "revision": "57ad06db9a294f577126fec16faf1ba1",
+    "url": "/my-portfolio/static/media/video.57ad06db.mp4"
+  }
+]);
